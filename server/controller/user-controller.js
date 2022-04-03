966ded9d35b4a7d217201fcb76da3228d21d31eb
vishalk0103 <kolekarv7899@gmail.com>
2022-04-03 12:24:51 +0530

    backend

--- server/controller/user-controller.js
+++ server/controller/user-controller.js
@@ -1,7 +1,7 @@
 const User = require("../model/user");
 const jwt = require("jsonwebtoken");
 const bcryptjs = require("bcryptjs");
-const HttpError=require('../middleware/http-error')
+const HttpError = require("../middleware/http-error");
 
 const getUserInfoById = async (req, res) => {
   const userId = req.params.userId;
@@ -9,15 +9,14 @@ const getUserInfoById = async (req, res) => {
   if (!indentifyUser) {
     return;
   }
-  res.json({username:indentifyUser.username,email:indentifyUser.email });
+  res.json({ username: indentifyUser.username, email: indentifyUser.email });
 };
 
-const signup = async (req, res,next) => {
+const signup = async (req, res, next) => {
   const { username, email, password } = req.body;
   const existUser = await User.findOne({ email: email });
   if (existUser) {
-    return next(
-      new HttpError('Email is already Exist!'))
+    return next(new HttpError("Email is already Exist!"));
   }
   let hashedpassword = await bcryptjs.hash(password, 12);
   try {
@@ -37,7 +36,7 @@ const signup = async (req, res,next) => {
     res.json({
       userId: newUser._id,
       email: newUser.email,
-      username:newUser.username,
+      username: newUser.username,
       token: token,
     });
     console.log(newUser);
@@ -46,21 +45,18 @@ const signup = async (req, res,next) => {
   }
 };
 
-const login = async (req, res ,next) => {
+const login = async (req, res, next) => {
   const { email, password } = req.body;
   const identifyUser = await User.findOne({ email: email });
   if (!identifyUser) {
-    return next(
-      new HttpError('Invalid credentials!'))
+    return next(new HttpError("Invalid credentials!"));
   }
-  let isValidPassword = await bcryptjs.compare(password, identifyUser.password)
-  console.log(identifyUser.password)
-  console.log()
+  let isValidPassword = await bcryptjs.compare(password, identifyUser.password);
+  console.log(identifyUser.password);
+  console.log();
   if (!isValidPassword) {
-    return next(
-      new HttpError('Invalid credentials!'))
+    return next(new HttpError("Invalid credentials!"));
   }
-  
 
   const token = jwt.sign(
     { userId: identifyUser._id, email: identifyUser.email },
@@ -70,11 +66,31 @@ const login = async (req, res ,next) => {
   res.json({
     userId: identifyUser._id,
     email: identifyUser.email,
-    name:identifyUser.username,
+    username: identifyUser.username,
+    token: token,
+  });
+};
+
+const editUser = async (req, res) => {
+  const { username, email } = req.body;
+  const userId = req.params.userId;
+  const user = await User.findById(userId);
+  user.email = email;
+  user.username = username;
+  await user.save();
+
+  const token = jwt.sign({ userId: user._id, email: user.email }, "dontshare", {
+    expiresIn: "1h",
+  });
+  res.json({
+    userId: user._id,
+    email: user.email,
+    username: user.username,
     token: token,
   });
 };
 
 exports.getUserInfoById = getUserInfoById;
+exports.editUser = editUser;
 exports.signup = signup;
 exports.login = login;
--- server/routes/user-route.js
+++ server/routes/user-route.js
@@ -1,9 +1,10 @@
-const express= require('express')
-const router= express.Router()
-const userController=require('../controller/user-controller')
+const express = require("express");
+const router = express.Router();
+const userController = require("../controller/user-controller");
 
-router.post('/signup',userController.signup)
-router.post('/login',userController.login)
-router.get('/:userId/profile',userController.getUserInfoById)
+router.post("/signup", userController.signup);
+router.post("/login", userController.login);
+router.get("/:userId/profile", userController.getUserInfoById);
+router.patch("/:userId", userController.editUser);
 
-module.exports=router
+module.exports = router;
